79e2cb2b3e592f27f37e82b161f7984fcb173394
axel puertas <axalpusa1125@gmail.com>
2023-02-21 19:34:21 -0500

    primeros pasos interface

---
+++ src/aplicacion.java
@@ -0,0 +1,12 @@
+import vews.operaciones;
+
+public class aplicacion {
+	public aplicacion() {
+		presentarVentana();
+	 }
+		
+	private void presentarVentana() {
+	operaciones miventana = new operaciones();
+		miventana.setVisible(true);
+	}
+}
---
+++ src/principal.java
@@ -0,0 +1,7 @@
+
+public class principal {
+	public static void main(String[] args) {
+		aplicacion miaplicacion=new aplicacion();
+		
+	}
+}
